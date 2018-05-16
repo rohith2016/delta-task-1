65ec10a2e40ed8bd3db2e2ba4429614ff77da327
rohith <38404850+rohith2016@users.noreply.github.com>
2018-05-16 22:43:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-
-var namev,idv,csv,wev,punv,knv,comv,ratv,x;
+var numberv, namev, idv, csv, wev, punv, knv, comv, ratv, x;
 function newmentee() {
-
 }
 
-function submit2(e){
+function submit2(e) {
     e.preventDefault();
     e.stopPropagation();
-    x=document.getElementById("details");
-    idv=x["ID"].value;
-    csv=x["CS"].value;
-    wev=x["WE"].value;
-    punv=x["pun"].value;
-    comv=x["com"].value;
-    knv=x["KN"].value;
-    ratv=x["rat"].value;
+    x = document.getElementById("details");
+   /* numberv = x["number"]. value;
+    namev = x["name"].value;
+    idv = x["ID"].value;
+    csv = x["CS"].value;
+    wev = x["WE"].value;
+    punv = x["pun"].value;
+    comv = x["com"].value;
+    knv = x["KN"].value;
+    ratv = x["rat"].value;*/
+
+    var y = document.createElement('div');
+    y.numberv=x["number"].value;
+    y.namev=x["name"].value;
+    y.idv=x["ID"].value;
+    y.csv=x["CS"].value;
+    y.wev=x["WE"].value;
+    y.punv=x["pun"].value;
+    y.comv=x["com"].value;
+    y.knv=x["KN"].value; 
+    y.ratv=x["rat"].value;
+    document.getElementById("container").appendChild(y);
     
-    document.getElementById("name").innerHTML += "name:" + namev + "<br>";
-    document.getElementById("ID").innerHTML += "ID:" + idv + "<br>";
-    document.getElementById("CS").innerHTML += "coding skills" + csv + "<br>";
-    document.getElementById("WE").innerHTML += "work etcs:" + wev + "<br>";
-    document.getElementById("pun").innerHTML += "punctuality:" + punv + "<br>";
-    document.getElementById("KN").innerHTML += "knowledge:" + knv + "<br>";
-    document.getElementById("com").innerHTML += "comments:" + comv + "<br>";
-    document.getElementById("rat").innerHTML += "rating:" + ratv + "<br>";
+    document.getElementById("number2").innerHTML = "number:" + numberv + "<br>";
+    document.getElementById("name2").innerHTML = "name:" + namev + "<br>";
+    document.getElementById("ID2").innerHTML = "ID:" + idv + "<br>";
+    document.getElementById("CS2").innerHTML = "coding skills" + csv + "<br>";
+    document.getElementById("WE2").innerHTML = "work etcs:" + wev + "<br>";
+    document.getElementById("pun2").innerHTML = "punctuality:" + punv + "<br>";
+    document.getElementById("KN2").innerHTML = "knowledge:" + knv + "<br>";
+    document.getElementById("com2").innerHTML = "comments:" + comv + "<br>";
+    document.getElementById("rat2").innerHTML = "rating:" + ratv + "<br>";
+
+        
+    /*document.getElementById("y").appendChild(namev);
+    document.getElementById("y").appendChild(idv);
+    document.getElementById("y").appendChild(csv);
+    document.getElementById("y").appendChild(wev);
+    document.getElementById("y").appendChild(punv);
+    document.getElementById("y").appendChild(knv);
+    document.getElementById("y").appendChild(comv);
+    document.getElementById("y").appendChild(ratv);*/
+    //document.getElementById("box").appendChild(x);
+    
     return false;
-    
+
+}
+function edit2() {
+    document.getElementById("details").contentEditable = true;
 }
 
 function colour() {
@@ -40,4 +68,6 @@ function colour() {
     else
         return red;
 }
-document.getElementById("details").addEventListener("submit",submit2,false);
+document.getElementById("details").addEventListener("submit", submit2, false);
+//document.getElementById("change").onclick = edit2();
+
