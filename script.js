303a3f9eddfdbb57eda4743a66b1b0d06ff9de05
rohith <38404850+rohith2016@users.noreply.github.com>
2018-05-19 03:21:03 +0530

    working webpage

--- script.js
+++ script.js
@@ -1,73 +1,120 @@
-var numberv, namev, idv, csv, wev, punv, knv, comv, ratv, x;
-function newmentee() {
-}
+ 
+ 
+ var swap;
+ var rating;
 
-function submit2(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    x = document.getElementById("details");
-   /* numberv = x["number"]. value;
-    namev = x["name"].value;
-    idv = x["ID"].value;
-    csv = x["CS"].value;
-    wev = x["WE"].value;
-    punv = x["pun"].value;
-    comv = x["com"].value;
-    knv = x["KN"].value;
-    ratv = x["rat"].value;*/
 
-    var y = document.createElement('div');
-    y.numberv=x["number"].value;
-    y.namev=x["name"].value;
-    y.idv=x["ID"].value;
-    y.csv=x["CS"].value;
-    y.wev=x["WE"].value;
-    y.punv=x["pun"].value;
-    y.comv=x["com"].value;
-    y.knv=x["KN"].value; 
-    y.ratv=x["rat"].value;
-    document.getElementById("container").appendChild(y);
-    
-    document.getElementById("number2").innerHTML = "number:" + y.numberv + "<br>";
-    document.getElementById("name2").innerHTML = "name:" + y.namev + "<br>";
-    document.getElementById("ID2").innerHTML = "ID:" + y.idv + "<br>";
-    document.getElementById("CS2").innerHTML = "coding skills" + y.csv + "<br>";
-    document.getElementById("WE2").innerHTML = "work etcs:" + y.wev + "<br>";
-    document.getElementById("pun2").innerHTML = "punctuality:" + y.punv + "<br>";
-    document.getElementById("KN2").innerHTML = "knowledge:" + y.knv + "<br>";
-    document.getElementById("com2").innerHTML = "comments:" + y.comv + "<br>";
-    document.getElementById("rat2").innerHTML = "rating:" + y.ratv + "<br>";
 
+
+ function add() {                                       
+    rating = document.getElementById("rating").value;
+    console.log(rating);
+    if(!(rating==1||rating==2||rating==3||rating==4||rating==5)){
+        alert("value exceeds 5. Please retry.")
+        return;
+        }
+    var Name = document.getElementById("name").value;
+    var rNo = document.getElementById("ID").value;
+    var comment=document.getElementById("comment").value;
+    var row=document.getElementById('container').insertRow(-1);
+    row.class="row";
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    var d=document.createElement("button");
+    var txtNode = document.createTextNode("Delete");
+    d.appendChild(txtNode);
+    d.setAttribute("onclick","del(this)");
+    row.appendChild(d);
+    var edit=document.createElement("button");
+    var node = document.createTextNode("Edit");
+    edit.appendChild(node);
+    edit.setAttribute("onclick","edit(this)");
+    edit.setAttribute("id","edit");
+    row.appendChild(edit);
+
+    row.style.backgroundColor=color();
+
+   
+    cell1.innerHTML=Name;
+    cell2.innerHTML=rNo;
+    cell3.innerHTML=comment;
+    cell4.innerHTML=rating;
+    cell3.title=comment;
+    cell1.title=Name;
+    swap=true;
         
-    /*document.getElementById("y").appendChild(namev);
-    document.getElementById("y").appendChild(idv);
-    document.getElementById("y").appendChild(csv);
-    document.getElementById("y").appendChild(wev);
-    document.getElementById("y").appendChild(punv);
-    document.getElementById("y").appendChild(knv);
-    document.getElementById("y").appendChild(comv);
-    document.getElementById("y").appendChild(ratv);*/
-    //document.getElementById("box").appendChild(x);
-    
-    return false;
+    } 
+ 
 
-}
-function edit2() {
-    document.getElementById("details").contentEditable = true;
-}
 
-function colour() {
-    if (ratv > 4)
-        return darkgreen;
-    else if (ratv > 3)
-        return green;
-    else if (ratv > 2)
-        return yellow;
-    else if (ratv > 1)
-        return orange;
-    else
-        return red;
-}
-document.getElementById("details").addEventListener("submit", submit2, false);
-//document.getElementById("change").onclick = edit2();
+ function del(row){   //enables deletimg a row
+     var line=row.parentNode.parentNode;
+     line.removeChild(row.parentNode);
+ 
+     }
 
+function edit(e){    //enables editing
+    var row=e.parentNode;
+    var button=document.getElementById("edit");
+    if (row.contentEditable == "true") {
+        row.contentEditable = "false";
+        var cells=row.getElementsByTagName('td');
+        rating=cells[3].innerHTML;
+        row.style.backgroundColor=color();
+        button.innerHTML = "Edit"; 
+        }
+    else {
+        row.contentEditable = "true";
+        button.innerHTML = "Submit";
+        row.style.backgroundColor="white";
+        }
+    }
+
+
+
+  function sort() {          
+      var table, rows, swap, i, row1, row2, condition;
+      table = document.getElementById("container");
+      swap = true;
+      while (swap) {
+          swap = false;
+          rows = table.getElementsByTagName("tr");
+          for (i = 1; i < (rows.length - 1); i++) {
+              condition = false;
+              row1 = rows[i].getElementsByTagName("td")[3];
+              row2 = rows[i + 1].getElementsByTagName("td")[3];
+      
+
+              if (Number(row1.innerHTML) > Number(row2.innerHTML)) {
+                  condition = true;
+                  break;
+                  }
+             }
+          if (condition) {
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+              swap = true;
+              }
+   
+          }
+     }
+
+
+ function  color() {  
+    var shade;
+    if(rating==1)
+        shade="red";
+    else if(rating==2)
+        shade="orange";
+    else if(rating==3)
+        shade="yellow ";
+    else if(rating==4)
+        shade="#adff2f";
+    else if(rating==5)
+        shade="green";
+
+    return shade;
+
+  }
+ 
